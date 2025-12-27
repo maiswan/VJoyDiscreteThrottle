75c05e17e8c341448b419642d8fc5248d737175f
maiswan <173935047+maiswan@users.noreply.github.com>
2025-12-27 11:00:31 +0800

    Add home route to let users know the app is alive

--- Maiswan.VJoyDiscreteThrottleServer/Program.cs
+++ Maiswan.VJoyDiscreteThrottleServer/Program.cs
@@ -7,18 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Read config
 const string configPath = "./configurations.json";
 string rawJson = File.ReadAllText(configPath);
-Configuration config = JsonSerializer.Deserialize<Configuration>(rawJson) ?? new();
+Configuration config = JsonSerializer.Deserialize<Configuration>(rawJson)
+    ?? throw new InvalidOperationException("Invalid configuration");
 
 // Initialize virtual throttle and conduct DI
-using DiscreteThrottle throttle = new(config.Notches.MaxBrake, config.Notches.MaxPower);
-builder.Services.AddSingleton<DiscreteThrottle>(throttle);
+using DiscreteThrottle throttle = new(config);
+builder.Services.AddSingleton(throttle);
 
 // Add services to the container.
 builder.Services.AddControllers();
 var app = builder.Build();
 
+// Home route
+app.MapGet("/", () => "maiswan/VJoyDiscreteThrottle");
+
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
-app.Run();
+app.Run($"http://localhost:{config.ServerPort}");
